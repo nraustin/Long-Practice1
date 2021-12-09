aa39fbbfe3183f44e4065a930c1d68955e6b7145
MasonKogami <maskogami@gmail.com>
2021-12-09 13:20:52 -0800

    problems 1 & 2 solved

--- problems/01-car-drive.js
+++ problems/01-car-drive.js
@@ -1,4 +1,25 @@
-// Your code here
+class Car {
+	constructor(speed = 250){
+		this.speed = speed;
+	}
+	drive (newSpeed) {
+		this.speed = newSpeed;
+		return this.speed;
+	}
+}
+
+let car = new Car();
+car.drive(0);     // => returns 0
+console.log(car); // => { speed: 0 }
+
+car.drive(10);    // => returns 10
+console.log(car); // => { speed: 10, }
+
+car.drive(50);    // => returns 50
+console.log(car); // -> { speed: 50 }
+
+car.drive(100);   // => returns 100
+console.log(car); // -> { speed: 100 }
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,5 +1,18 @@
-// Your code here
-
+class Calculator {
+	constructor () {
+		this.total = 0;
+	}
+	add = num => this.total += num
+	subtract = num => this.total -= num
+	multiply = num => this.total *= num
+	divide = num => this.total /= num
+}
+let calculator = new Calculator();
+console.log(calculator.add(50));      // => 50
+console.log(calculator.subtract(35)); // => 15
+console.log(calculator.multiply(10)); // => 150
+console.log(calculator.divide(5));    // => 30
+console.log(calculator.total)         // => 30
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
